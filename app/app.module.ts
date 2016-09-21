4f1ec5cbde675c899229ba820bd81bf4c3d7204b
marcosocon <oconmarcos97@gmail.com>
2016-09-21 09:31:52 -0300

    setted the first nested components

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { NavbarComponent }  from './components/navbar/navbar.component';
+
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+	imports: [ BrowserModule ],
+	declarations: [ AppComponent, NavbarComponent ],
+	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
