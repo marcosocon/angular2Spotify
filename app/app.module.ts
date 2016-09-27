f8d18ad4362977b491521346e5a06f748894cc2f
marcosocon <oconmarcos97@gmail.com>
2016-09-27 12:07:26 -0300

    added http module and spotify service to fetch data and some views to display it

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { routes, appRoutingProviders }  from './app.routes';
+import { SpotifyService } from './services/spotify.service';
+
 
 import { NavbarComponent }  from './components/navbar/navbar.component';
 import { AboutComponent }  from './components/about/about.component';
@@ -11,9 +15,9 @@ import { SearchComponent }  from './components/search/search.component';
 
 
 @NgModule({
-	imports: [ BrowserModule, routes ],
+	imports: [ BrowserModule, FormsModule, HttpModule, routes ],
 	declarations: [ AppComponent, NavbarComponent, AboutComponent, SearchComponent ],
-	providers: [ appRoutingProviders ],
+	providers: [ appRoutingProviders, SpotifyService ],
 	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- app/components/search/search.component.ts
+++ app/components/search/search.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Http, Headers } from '@angular/http';
+import { SpotifyService } from '../../services/spotify.service';
+import { Artist } from '../,,/../Artist';
+ 
 @Component({
 	moduleId: module.id,
 	selector: 'search',
-	templateUrl: 'search.component.html'
+	templateUrl: 'search.component.html',
+	providers: [SpotifyService]
 })
 
-export class SearchComponent { }
+export class SearchComponent {
+	searchStr: string;
+	results: Artist[];
+
+	constructor(private _spotifyService: SpotifyService){
+
+	}
+
+	searchMusic(){
+		this._spotifyService.searchMusic(this.searchStr)
+			.subscribe(res =>{
+				this.results = res.artists.items;
+			})
+
+	}
+}
